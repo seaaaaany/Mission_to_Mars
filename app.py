553d4224f7b25ed2c300cf0087a92704e6e274de
Sean <yycyjqc@gmail.com>
2018-05-10 15:55:40 -0700

    nothing change

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, jsonify, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 mongo = PyMongo(app)
 
